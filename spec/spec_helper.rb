86602fdfa24e52351f6027a2b1aa3394ed016a90
Your Name <you@example.com>
2017-03-26 18:40:36 -0400

    adding files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'hiera'
 
-base_path = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-   c.module_path = base_path
-   c.manifest_dir = File.join(base_path, 'manifests')
-   c.hiera_config = File.join(base_path, 'spec', 'fixtures', 'hiera.yaml')
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.hiera_config = File.join(fixture_path, 'hiera.yaml')
 end
